c1fb5ad0c83874c722ea22b7b7df48c071494a68
devdhruvper <121749576+devdhruvper@users.noreply.github.com>
2023-05-25 03:16:29 +0530

    Update Fetch_DT.py
    made the changes in JSON

--- Fetch_DT.py
+++ Fetch_DT.py
@@ -3,27 +3,32 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from ordered_set import OrderedSet
+data = []
+for Year in range(1950, 2024):
 
-number=input("Enter the year you want to fetch the data for : ")
-Year_URL=f"https://indiankanoon.org/browse/supremecourt/{number}/"
+ Year_URL = f"https://indiankanoon.org/browse/supremecourt/{Year}/"
 
-def multi_page(URL):
- url = OrderedSet()
- while True:
-  if URL not in url:
-   pass
-  else:
-   break
-  url.add(URL)
-  R = requests.get(URL)
-  HtmlContent = R.content
-  # HTML CONTENT HAI YEH RAW
-  soup = BeautifulSoup(HtmlContent, 'html.parser')
-  bottom_div = soup.find('div', {'class': 'bottom'})
-  URL = "https://indiankanoon.org" + bottom_div.find_all('a')[-1]['href']
- return url
 
-def extract_number_from_href(href):
+
+ def multi_page(URL):
+  url = OrderedSet()
+  while True:
+   if URL not in url:
+    pass
+   else:
+    break
+   url.add(URL)
+   R = requests.get(URL)
+   HtmlContent = R.content
+   # HTML CONTENT HAI YEH RAW
+   soup = BeautifulSoup(HtmlContent, 'html.parser')
+   bottom_div = soup.find('div', {'class': 'bottom'})
+   if bottom_div.find('a') is not None:
+    URL = "https://indiankanoon.org" + bottom_div.find_all('a')[-1]['href']
+  return url
+
+
+ def extract_number_from_href(href):
   pattern = r"/docfragment/(\d+)/"
   match = re.search(pattern, href)
   if match:
@@ -33,36 +38,38 @@ def extract_number_from_href(href):
    return None
 
 
+ R = requests.get(Year_URL)
+ Htmlcontent = R.content
+ soup = BeautifulSoup(Htmlcontent, 'html.parser')
+ Months_URL = []
+ for div in soup.find_all('div', {'class': 'browselist'}):
+  Months_URL.append("https://indiankanoon.org" + div.find('a')['href'].replace(" ", "%20"))
 
-
-R=requests.get(Year_URL)
-Htmlcontent=R.content
-soup=BeautifulSoup(Htmlcontent,'html.parser')
-Months_URL=[]
-for div in soup.find_all('div',{'class':'browselist'}):
- Months_URL.append("https://indiankanoon.org"+div.find('a')['href'].replace(" ","%20"))
-
-
-for start_URL in Months_URL:
- urls = list(multi_page(start_URL))
- for URL in urls:
-  Link_container = []
-  R = requests.get(URL)
-  HtmlContent = R.content
-  soup = BeautifulSoup(HtmlContent, 'html.parser')
-
-  for div in soup.find_all('div', {'class': 'result_title'}):
-   for link in div.find_all('a'):
-    number = extract_number_from_href(link.get('href'))
-    links = f'https://indiankanoon.org//doc/{number}/'
-    Link_container.append(links)
-
-
-  for URL in Link_container:
+ i = 0
+ for start_URL in Months_URL:
+  urls = list(multi_page(start_URL))
+  for URL in urls:
+   i = i + 1;
+   Link_container = []
+   numbers = []
    R = requests.get(URL)
    HtmlContent = R.content
    soup = BeautifulSoup(HtmlContent, 'html.parser')
-   page_div = soup.find("div", class_="judgments")
-   ad_doc = page_div.find("div", class_="ad_doc")
-   ad_doc.decompose()
-   print(page_div.text)
+
+   for div in soup.find_all('div', {'class': 'result_title'}):
+    for link in div.find_all('a'):
+     number = extract_number_from_href(link.get('href'))
+     links = f'https://indiankanoon.org//doc/{number}/'
+     Link_container.append(links)
+     numbers.append(number)
+
+   for URL, no in zip(Link_container, numbers):
+    R = requests.get(URL)
+    HtmlContent = R.content
+    soup = BeautifulSoup(HtmlContent, 'html.parser')
+    page_div = soup.find("div", class_="judgments")
+    tagline = page_div.find("div", class_="doc_title").text
+    ad_doc = page_div.find("div", class_="ad_doc")
+    ad_doc.decompose()
+    data.append({'id': no, 'date': f"{i}/{Year}", 'tagline': tagline})
+print(data)
